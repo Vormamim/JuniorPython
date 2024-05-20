3e291038898a95647a5bc06940f7cfc901f1bc08
Vormamim <81339757+Vormamim@users.noreply.github.com>
2024-05-20 10:41:22 +1000

    Create 2_PREDICT_main.py
    this is the python file for this example

---
+++ 2_PREDUCT_main.py
@@ -0,0 +1,21 @@
+#Predict and Run Example 1
+# Add comments to the code below to explain what it does
+
+firstName = "Andy"
+
+print(firstName)
+
+#Predict and Run Example 2
+ # Add comments to the code below to explain what it does
+
+lastName = "Pandy"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+#Predict and Run Example 3
+ # Add comments to the code below to explain what it does
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
+
